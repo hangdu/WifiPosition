a1f3b812047dabfb4fa632088edc4fff74f7bef8
hangdu <2580392278@qq.com>
2017-09-25 14:00:34 -0400

    support to show the real time AP scan result

--- app/src/main/java/com/example/hang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hang/myapplication/MainActivity.java
@@ -15,17 +15,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     final private int REQUEST_CODE_ASK_PERMISSIONS = 1234;
     TextView textView;
+    Map<String, Integer> pos1Finger;
+    Map<String, Integer> pos2Finger;
+    WifiManager wifiManager;
+    int scanTimesForOnePoint = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MyApp","I am here");
-
+        pos1Finger = new HashMap<>();
+        pos2Finger = new HashMap<>();
 
         //Check for Android M runtime permissions
         if(Build.VERSION.SDK_INT >= 23) {
@@ -43,18 +50,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 // Level of a Scan Result
-        List<ScanResult> wifiList = wifiManager.getScanResults();
-        for (ScanResult scanResult : wifiList) {
-            int level = WifiManager.calculateSignalLevel(scanResult.level, 5);
-            String bssid = scanResult.BSSID;
-            String networkName = scanResult.SSID;
-            int rssi = scanResult.level;
-            System.out.println("ScanResult BSSID is " + bssid);
-            System.out.println("ScanResult rssi is " + rssi);
-            System.out.println("ScanResult networkName is " + networkName);
-        }
+//        List<ScanResult> wifiList = wifiManager.getScanResults();
+//        for (ScanResult scanResult : wifiList) {
+//            int level = WifiManager.calculateSignalLevel(scanResult.level, 5);
+//            String bssid = scanResult.BSSID;
+//            String networkName = scanResult.SSID;
+//            int rssi = scanResult.level;
+//            System.out.println("ScanResult BSSID is " + bssid);
+//            System.out.println("ScanResult rssi is " + rssi);
+//            System.out.println("ScanResult networkName is " + networkName);
+//        }
 
 
         textView = (TextView) findViewById(R.id.textview_result);
@@ -63,6 +70,28 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textView.setText("Button1 is clicked");
+                pos1Finger.clear();
+                List<ScanResult> wifiList = wifiManager.getScanResults();
+                System.out.println("Scanning result for the first position");
+                StringBuilder builder = new StringBuilder();
+                builder.append("In total AP number = " + wifiList.size() +  "\n");
+                builder.append("\n");
+                for (ScanResult scanResult : wifiList) {
+                    String bssid = scanResult.BSSID;
+                    String networkName = scanResult.SSID;
+                    int rssi = scanResult.level;
+                    System.out.println("NetworkName = " + networkName);
+                    System.out.println("Mac address = " + bssid);
+                    System.out.println("Rssi = " + rssi);
+
+                    builder.append("NetworkName = " + networkName + "\n");
+                    builder.append("Mac address = " + bssid+ "\n");
+                    builder.append("Rssi = " + rssi+ "\n");
+                    builder.append("\n");
+
+                    pos1Finger.put(bssid, rssi);
+                }
+                textView.setText(builder.toString());
             }
         });
 
@@ -72,12 +101,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 textView.setText("Button2 is clicked");
+
+                pos2Finger.clear();
+                List<ScanResult> wifiList = wifiManager.getScanResults();
+                System.out.println("Scanning result for the first position");
+                StringBuilder builder = new StringBuilder();
+                builder.append("In total AP number = " + wifiList.size() +  "\n");
+                builder.append("\n");
+                for (ScanResult scanResult : wifiList) {
+                    String bssid = scanResult.BSSID;
+                    String networkName = scanResult.SSID;
+                    int rssi = scanResult.level;
+                    System.out.println("NetworkName = " + networkName);
+                    System.out.println("Mac address = " + bssid);
+                    System.out.println("Rssi = " + rssi);
+
+                    builder.append("NetworkName = " + networkName + "\n");
+                    builder.append("Mac address = " + bssid+ "\n");
+                    builder.append("Rssi = " + rssi+ "\n");
+                    builder.append("\n");
+
+                    pos2Finger.put(bssid, rssi);
+                }
+                textView.setText(builder.toString());
             }
         });
-
-
-
-
-
     }
 }
