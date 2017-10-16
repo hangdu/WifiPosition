223da08bd657029bb23bb9088cbed4a2bbc10ec1
hangdu <2580392278@qq.com>
2017-10-16 19:45:27 -0400

    data can be saved to database in server part

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 import socket               # Import socket module
 import json
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+conn.execute('''CREATE TABLE IF NOT EXISTS FINGER
+         (ReferencePoint CHAR(50) NOT NULL,
+         MacAddress           TEXT    NOT NULL,
+         Intensity1            INT     NOT NULL,
+         Intensity2            INT     NOT NULL,
+         Intensity3            INT     NOT NULL,
+         Intensity4            INT     NOT NULL,
+         Intensity5            INT     NOT NULL);''')
 
 s = socket.socket()         # Create a socket object
 #host = socket.gethostname() # Get local machine name
@@ -18,5 +29,15 @@ while True:
    referencePoint = dict['position']
    print('referencePoint is ' + referencePoint)
    print(dict['map'])
-   #print('name is' + dict['Name'])
+   #You can insert 3 records (3 rows)
+   
+   map1 = dict['map']
+   for macAddress in map1:
+      intensityList = map1[macAddress]
+      conn.execute("INSERT INTO FINGER (ReferencePoint,MacAddress,Intensity1,Intensity2,Intensity3, Intensity4, Intensity5) \
+      VALUES (?, ?, ?, ?, ?, ?, ?)", (referencePoint, macAddress, intensityList[0], intensityList[1], intensityList[2], intensityList[3], intensityList[4]));
+      conn.commit()
+   
    c.close()                # Close the connection
+
+conn.close()
