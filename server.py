57fa56e4e17111c40c6b4fb9ba78494ffcb75229
hangdu <2580392278@qq.com>
2017-10-20 16:49:47 -0400

    refer1 AP1 get a proby

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ import socket               # Import socket module
 import json
 import sqlite3
 import statistics
+from scipy.stats import norm
 from subprocess import check_output
 
 
 conn = sqlite3.connect('test.db')
-conn.execute('''CREATE TABLE IF NOT EXISTS FINGER
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS FINGER
          (ReferencePoint CHAR(50) NOT NULL,
          MacAddress           TEXT    NOT NULL,
          mean                  REAL    NOT NULL,
@@ -16,11 +18,13 @@ conn.execute('''CREATE TABLE IF NOT EXISTS FINGER
          Intensity3            INT     NOT NULL,
          Intensity4            INT     NOT NULL,
          Intensity5            INT     NOT NULL);''')
-conn.execute('''CREATE TABLE IF NOT EXISTS REFERENCEPOSITIONS
+cursor.execute('''CREATE TABLE IF NOT EXISTS REFERENCEPOSITIONS
          (ReferencePoint CHAR(50)  NOT NULL,
          MacAddress1     TEXT      NOT NULL,
          MacAddress2     TEXT      NOT NULL,
          MacAddress3     TEXT      NOT NULL);''')
+
+
 s = socket.socket()         # Create a socket object
 ips = check_output(['hostname', '--all-ip-addresses']).decode("utf-8")
 host = ips.split(' ')[0]
@@ -59,8 +63,9 @@ while True:
          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (referencePoint, macAddress, m, std, intensityList[0], intensityList[1], intensityList[2], intensityList[3], intensityList[4]));
          conn.commit()
 
+      macList.sort()
       print(macList)
-      conn.execute("INSERT INTO REFERENCEPOSITIONS (ReferencePoint,MacAddress1,MacAddress2,MacAddress3) \
+      cursor.execute("INSERT INTO REFERENCEPOSITIONS (ReferencePoint,MacAddress1,MacAddress2,MacAddress3) \
       VALUES (?,?,?,?)", (referencePoint,macList[0],macList[1],macList[2]));
       conn.commit()
    
@@ -73,8 +78,33 @@ while True:
       for macAddress in map1:
          l.append(macAddress)
 
+      l.sort()
       myString = ",".join(l)
       c.send(myString.encode())
+      cursor.execute("select ReferencePoint from REFERENCEPOSITIONS where MacAddress1 = ? and MacAddress2 = ? and MacAddress3 = ?", (l[0], l[1], l[2]))
+      conn.commit()
+      rows = cursor.fetchall()
+      print(rows)
+      for row in rows:
+         print(row)
+         #row is a tuple
+         referP = row[0]
+         #referP  AP1
+         cursor.execute("select mean, std from FINGER where ReferencePoint = ? and MacAddress = ?", (referP, l[0]))
+         conn.commit()
+         tmp = cursor.fetchall()[0]
+         mean = tmp[0]
+         std = tmp[1]
+         
+         print('mean='+str(mean))
+         print('std='+str(std))
+         if std == 0:
+            std = 0.5
+         intensityList = map1[l[0]]
+         p1 = norm.pdf(statistics.mean(intensityList), mean, std)
+         print('probalilaty for the first AP is' + str(p1))
+      #go through table referencePositions to get all the reference points which have the same 3 AP information
+      #Sort
 
    c.close()                # Close the connection
 
